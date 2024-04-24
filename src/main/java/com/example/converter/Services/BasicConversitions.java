170a4431b8329ebc56aa54dd9c45f8a34aa1c60a
elangova <Elangova@corp.untd.com>
2024-04-24 17:30:05 +0530

    Fixed base64 conversions and implemented jwt conversions

--- src/main/java/com/example/converter/Services/BasicConversitions.java
+++ src/main/java/com/example/converter/Services/BasicConversitions.java
@@ -5,6 +5,8 @@ import java.net.URLDecoder;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.stereotype.Service;
 
@@ -14,12 +16,13 @@ import org.springframework.web.context.annotation.RequestScope;
 @RequestScope
 public class BasicConversitions {
     
-    public String base64Encode(String param) {
+    public String base64Encode(String param) throws UnsupportedEncodingException {
 
-        String encodedString = Base64.getEncoder().encodeToString(param.getBytes());
+        String encodedString = Base64.getEncoder().encodeToString(param.getBytes("UTF-8"));
         System.out.println("Encoded String:"+encodedString);
         return encodedString;
-    }
+
+    }//DONE
 
     /*public static byte[] base64Encode2(byte[] data) throws UnsupportedEncodingException {
         return Base64.decodeBase64(data);
@@ -30,13 +33,15 @@ public class BasicConversitions {
     }*/
     
 
-    public String base64Decode(String param) {
+    public String base64Decode(String param) throws UnsupportedEncodingException {
 
         byte[] decodedBytes = Base64.getDecoder().decode(param);
-        String decodedString = new String(decodedBytes);
+        String decodedString = new String(decodedBytes,"UTF-8");
         System.out.println("Decoded String:"+decodedString);
         return decodedString;
-    }
+
+        //System.out.println("Decoded: " + new String(bytes, "UTF-8")); /
+    }////DONE
 
     public String URLDecoder(String value,String method) throws UnsupportedEncodingException {
 
@@ -94,7 +99,31 @@ public class BasicConversitions {
         return encodedString;
     }
 
-    public String jwtDecode(){
-        return "hi";
+    public Map<String, Object> jwtDecode(String token) throws UnsupportedEncodingException{
+
+        System.out.println("received token:{}"+token);
+        String parts[] = token.split("\\.");
+
+        if( parts.length < 2 || parts.length >3){
+            System.out.println("not a proper jwt Token"+token+"length is:"+parts.length);            
+        }
+
+        String header = base64Decode(parts[0]);
+        String payload =  base64Decode(parts[1]);
+        String signature=null;
+
+        if(parts.length==3){
+            signature  = base64Decode(parts[2]);
+        }
+        Map<String,Object> decodedJwtMap= new HashMap<String,Object>();   
+        decodedJwtMap.put("header", header);
+        decodedJwtMap.put("payload", payload);
+        decodedJwtMap.put("signature", signature);
+
+        System.out.println("decodedJwtMap:{}"+decodedJwtMap);
+        return decodedJwtMap;
+
+        //        JSONObject header = new JSONObject(decode(parts[0]));
+        //  JSONObject payload = new JSONObject(decode(parts[1]));
     }
 }
